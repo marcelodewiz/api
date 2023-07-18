3ae8d1e31db4e2e49d703aa32ede80521a14b55e
Marcelo Migotto <marcelo.dewiz@gmail.com>
2023-07-17 23:25:03 -0300

    Aula 1 Boas práticas na API

---
+++ src/main/java/med/escaioni/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,19 @@
+package med.escaioni.api.paciente;
+
+import med.escaioni.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getTelefone(),
+                paciente.getCpf(),
+                paciente.getEndereco());
+    }
+}
